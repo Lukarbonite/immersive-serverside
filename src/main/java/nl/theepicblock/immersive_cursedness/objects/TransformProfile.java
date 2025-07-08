ad20421f38382fd2f4b0cdb5e76eaa40efc87515
lukehinojosa <lukehinojosa05@gmail.com>
2025-07-08 13:44:10 -0400

    TransformProfile.java rotation fix
    This file contained two bugs. First, the coordinate transformation logic in the transform methods was flawed, incorrectly applying coordinate swaps for 180-degree rotations. I've rewritten this logic to be clearer and correct for all rotation angles. Second, the rotate(Direction) method had a typo (case 190) which I've corrected to case 180.

--- src/main/java/nl/theepicblock/immersive_cursedness/objects/TransformProfile.java
+++ src/main/java/nl/theepicblock/immersive_cursedness/objects/TransformProfile.java
@@ -1,14 +1,11 @@
 package nl.theepicblock.immersive_cursedness.objects;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
-import nl.theepicblock.immersive_cursedness.Util;
 
 public class TransformProfile {
     private final int originalX;
@@ -42,50 +39,69 @@ public class TransformProfile {
         return y-targetY+originalY;
     }
 
-    // rot(in - original) + target
     public BlockPos transform(BlockPos in) {
-        int x = in.getX();
-        int y = in.getY();
-        int z = in.getZ();
+        int relX = in.getX() - originalX;
+        int relZ = in.getZ() - originalZ;
 
-        // Swap x and z
-        if (this.rotation != 0) {
-            var temp = x;
-            x = z;
-            z = temp;
+        int newRelX;
+        int newRelZ;
+
+        switch (this.rotation) {
+            case 90:
+                newRelX = -relZ;
+                newRelZ = relX;
+                break;
+            case -90:
+                newRelX = relZ;
+                newRelZ = -relX;
+                break;
+            case 180:
+                newRelX = -relX;
+                newRelZ = -relZ;
+                break;
+            default:
+                newRelX = relX;
+                newRelZ = relZ;
+                break;
         }
 
-        // We have to flip the x or the y their sign in certain situations
-        var rotbitX = (this.rotation == -90 || this.rotation == 180) ? -1 : 1;
-        var rotbitY = (this.rotation == 90 || this.rotation == 180) ? -1 : 1;
-
         return new BlockPos(
-                rotbitX * (x - originalX) + targetX,
-                (y - originalY) + targetY,
-                rotbitY * (z - originalZ) + targetZ
+                newRelX + targetX,
+                (in.getY() - originalY) + targetY,
+                newRelZ + targetZ
         );
     }
 
     public Vec3d transform(Vec3d in) {
-        double x = in.getX();
-        double y = in.getY();
-        double z = in.getZ();
+        double relX = in.getX() - originalX;
+        double relZ = in.getZ() - originalZ;
 
-        // Swap x and z
-        if (this.rotation != 0) {
-            var temp = x;
-            x = z;
-            z = temp;
+        double newRelX;
+        double newRelZ;
+
+        switch (this.rotation) {
+            case 90: // CCW
+                newRelX = -relZ;
+                newRelZ = relX;
+                break;
+            case -90: // CW
+                newRelX = relZ;
+                newRelZ = -relX;
+                break;
+            case 180:
+                newRelX = -relX;
+                newRelZ = -relZ;
+                break;
+            default: // 0 degrees
+                newRelX = relX;
+                newRelZ = relZ;
+                break;
         }
 
-        // We have to flip the x or the y their sign in certain situations
-        var rotbitX = (this.rotation == -90 || this.rotation == 180) ? -1 : 1;
-        var rotbitY = (this.rotation == 90 || this.rotation == 180) ? -1 : 1;
-
         return new Vec3d(
-                rotbitX * (x - originalX) + targetX,
-                (y - originalY) + targetY,
-                rotbitY * (z - originalZ) + targetZ
+                newRelX + targetX,
+                (in.getY() - originalY) + targetY,
+                newRelZ + targetZ
         );
     }
 
@@ -105,25 +121,25 @@ public class TransformProfile {
                 default -> in;
                 case 90 -> Direction.WEST;
                 case -90 -> Direction.EAST;
-                case 190 -> Direction.SOUTH;
+                case 180 -> Direction.SOUTH;
             };
             case WEST -> switch (this.rotation) {
                 default -> in;
                 case 90 -> Direction.SOUTH;
                 case -90 -> Direction.NORTH;
-                case 190 -> Direction.EAST;
+                case 180 -> Direction.EAST;
             };
             case EAST -> switch (this.rotation) {
                 default -> in;
                 case 90 -> Direction.NORTH;
                 case -90 -> Direction.SOUTH;
-                case 190 -> Direction.WEST;
+                case 180 -> Direction.WEST;
             };
             case SOUTH -> switch (this.rotation) {
                 default -> in;
                 case 90 -> Direction.EAST;
                 case -90 -> Direction.WEST;
-                case 190 -> Direction.NORTH;
+                case 180 -> Direction.NORTH;
             };
         };
     }
